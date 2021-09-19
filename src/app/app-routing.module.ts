043fef55cf587cd146b70466a750e47a70f46678
Hugo Jeller Ferreira <hugo_jeller@hotmail.com>
2021-09-19 17:11:29 -0400

    Redirect root to posts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [{
-  path: 'posts',
-  loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule)
-}, {
-  path: 'tags',
-  loadChildren: () => import('./tags/tags.module').then(m => m.TagsModule)
-}];
+const routes: Routes = [
+  {
+    path: 'posts',
+    loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule)
+  }, {
+    path: 'tags',
+    loadChildren: () => import('./tags/tags.module').then(m => m.TagsModule)
+  }, {
+    path: '**',
+    redirectTo: '/posts',
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
