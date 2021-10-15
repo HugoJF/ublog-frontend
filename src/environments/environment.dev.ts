becfa02ddd5999ae731b47a03928d321787b8493
Hugo Jeller Ferreira <hugo_jeller@hotmail.com>
2021-10-15 19:26:50 -0400

    Added missing environment variables

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://9scernu61i.execute-api.us-east-2.amazonaws.com'
+  baseUrl: 'https://9scernu61i.execute-api.us-east-2.amazonaws.com',
+  cognitoPoolId: 'us-east-1_Z5Cw4Zq7c',
+  cognitoClientId: 'v2ki29kbh7q9po090ti70nuo7',
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  cognitoPoolId: 'us-east-1_Z5Cw4Zq7c',
+  cognitoClientId: 'v2ki29kbh7q9po090ti70nuo7',
 };
